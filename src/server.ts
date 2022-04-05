3d2cdc6ad5db25e6b3486f8f39b182b3c6a7924c
Rubens de Deus Meirelles <rubens.meirelles@postalsaude.com.br>
2022-04-05 18:45:49 -0300

    projeto final 1.0

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ server.use(express.static(path.join(__dirname, '../public')));
 server.use(mainRoutes);
 
 server.use((req, res) => {
-    res.send('Página não entontrada :(')
+    res.render('pages/404')
 })
 
 server.listen(process.env.PORT);
